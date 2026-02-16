804300251437d9e0ba5c55bdf4441eaa589c6a7f
Yash Dadlani <dadlaniyash2004@gmail.com>
2026-02-16 20:36:54 +0530

    EASYEasy

---
+++ 190.Reverse Bits.py
@@ -0,0 +1,15 @@
+# 16/02/2026
+
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        if n == 0:
+            return 0
+        
+        result = 0
+        for _ in range(32):
+            result <<= 1          
+            result = result | (n & 1)
+            n >>= 1               # n = n >> 1
+        
+        return result
+    
